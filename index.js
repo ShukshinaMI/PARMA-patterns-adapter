bacedd135782622e0801a0585a98e53c784c5e51
ShukshinaMI <74698961+ShukshinaMI@users.noreply.github.com>
2023-01-13 12:30:10 +0500

    add adapter

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
-// Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const oldCost = document.querySelector('.old-cost');
+const newCost = document.querySelector('.new-cost');
+
+function Shipping() {
+  this.request = function (zipStart, zipEnd, weight) {
+    const rate = 49.75;
+    return `$${rate} x ${weight} = ${rate * weight}`;
+  };
+}
+
+function AdvancedShipping() {
+  this.login = function (credentials) {};
+
+  this.setStart = function (start) {};
+
+  this.setDestination = function (destination) {};
+
+  this.calculate = function (weight) {
+    const rate = 39.5;
+    return `$${rate} x ${weight} = ${rate * weight}`;
+  };
+}
+
+function ShippingAdapter(credentials) {
+  const shipping = new AdvancedShipping();
+
+  shipping.login(credentials);
+
+  return {
+    request: function (zipStart, zipEnd, weight) {
+      shipping.setStart(zipStart);
+      shipping.setDestination(zipEnd);
+      return shipping.calculate(weight);
+    },
+  };
+}
+
+function getCost() {
+  const shipping = new Shipping();
+  const credentials = {
+    token: '30a8-6ee1',
+  };
+  const adapter = new ShippingAdapter(credentials);
+
+  let cost = shipping.request('78701', '10010', 2);
+  oldCost.textContent = `${oldCost.textContent} ${cost}`;
+
+  cost = adapter.request('78701', '10010', 2);
+  newCost.textContent = `${newCost.textContent} ${cost}`;
+}
+
+getCost();
